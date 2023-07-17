b85ddf985f78970bd87b1d0d2cce2a94bbe39271
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-17 13:03:23 +0530

    chore: use custom db number for database, format file

--- server/database/redis.go
+++ server/database/redis.go
@@ -9,11 +9,11 @@ import (
 
 var Ctx = context.Background()
 
-func CreateClient() *redis.Client {
+func CreateClient(dbNo int) *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("DB_ADDRESS"),
+		Addr:     os.Getenv("DB_ADDRESS"),
 		Password: os.Getenv("DB_PASSWORD"),
-		DB: 0,
+		DB:       dbNo,
 	})
 
 	return client
