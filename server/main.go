76ededafc5194e522f004c311ff6d64eb4097ee0
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-17 15:38:52 +0530

    feat: instantiate new fiber app, handle post request to api/v1 route

--- server/main.go
+++ server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/SazedWorldbringer/url-shortener/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Post("/api/v1", routes.Shorten)
+}
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load environment file.")
+	}
+
+	app := fiber.New()
+	app.Use(logger.New())
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":4000"))
 }
